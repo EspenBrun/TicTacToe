b9a595da6f0c5559e881390e51413befe97ff4cd
Espen K. Brun <espen_brun@hotmail.com>
2017-04-30 12:09:09 +0200

    almost workin, but get List from containsAll, cant get to ArraList. GOnna try to convert all to List

--- app/src/main/java/com/example/android/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/android/tictactoe/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.android.tictactoe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import java.util.Collection;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void ok(View v){
         Intent i = new Intent("ebru.playersActivity");
         startActivity(i);
+        test();
+
+    }
+
+    public void test(){
+        ArrayList<Integer> list = new ArrayList<>();
+//        int[] win = {1,2,3};
+//        int[] list = {0,1,3,0};
+       ArrayList<Integer> win = new ArrayList<>();
+        win.add(0);
+        win.add(1);
+        win.add(2);
+
+        list.add(2);
+        list.add(1);
+        list.add(3);
+        list.add(0);
+
+        boolean worked = false;
+
+        worked = list.containsAll(win);
+        Log.i("trying", "it worked: " + worked);
     }
 }
--- app/src/main/java/com/example/android/tictactoe/TicTacToeActivity.java
+++ app/src/main/java/com/example/android/tictactoe/TicTacToeActivity.java
@@ -7,8 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 /**
@@ -24,14 +27,37 @@ public class TicTacToeActivity extends AppCompatActivity {
     private int[] btnIds;
     private ArrayList<Integer> p1Btns = new ArrayList<>();
     private ArrayList<Integer> p2Btns = new ArrayList<>();
+    private ArrayList<Integer> winCombIds = new ArrayList<>();
+    // three and three indicates a possible winning combination on a 3x3 board numbered from 0-8
+    private final int NUM_WINS_COMB = 8;
+    private final ArrayList<Integer> WIN_COMB = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,0,3,6,1,4,7,2,5,8,0,4,8,2,4,6));
+    private boolean p1Won = false;
+    private boolean p2Won = false;
+    private TextView winnerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe);
+
+        winnerView = (TextView) findViewById(R.id.winnerView);
         res = getResources();
         getBtnIds();
+        getPossibleWins();
 
+    }
+
+    public boolean hasWon(ArrayList pBtns){
+        int i = 0;
+        boolean hasWon = false;
+        Log.i(TAG,"pBtns:" + pBtns.toString());
+        while(!hasWon && i<NUM_WINS_COMB){
+            List<Integer> subList = new List<String>(winCombIds.subList(i,i+3));
+            hasWon = pBtns.containsAll(subList);
+            Log.i(TAG, "hasWon: " + hasWon + ", the ids i check are " + winCombIds.subList(3*i,3*i+3).toString());
+            i++;
+        }
+        return hasWon;
     }
 
     public void getBtnIds(){
@@ -39,16 +65,31 @@ public class TicTacToeActivity extends AppCompatActivity {
         Log.i(TAG,"length " + btnIds.length);
         for (int i = 0; i < btnIds.length; i++) {
             btnIds[i] = res.getIdentifier("b"+i, "id", getPackageName());
-            Log.i(TAG,"id: " + btnIds[i]);
         }
     }
+
+    public void getPossibleWins(){
+        for(int tile : WIN_COMB){
+            winCombIds.add(btnIds[tile]);
+        }
+//        Log.i(TAG,"The button ids: " + Arrays.toString(btnIds));
+//        Log.i(TAG,"All the ids in the win ids: " + winCombIds.toString());
+    }
+
     public void btnClick(View v){
         int id = v.getId();
         b = (Button) v;
-        if(spotTaken(id)){
-
-        } else {
+        if(p1Won || p2Won) { }
+        else if (spotTaken(id)) { }
+        else {
             playerMove(id);
+        }
+        if (p1Won){
+            winnerView.setText("P1 won!");
+            winnerView.setVisibility(View.VISIBLE);
+        } else if(p2Won){
+            winnerView.setText("P2 won!");
+            winnerView.setVisibility(View.VISIBLE);
         }
     }
 
@@ -61,11 +102,15 @@ public class TicTacToeActivity extends AppCompatActivity {
             b.setText("X");
             b.setTextColor(res.getColor(R.color.colorPlayerOne));
             p1Btns.add(id);
+            p1Won = hasWon(p1Btns);
+            Log.i(TAG,"p1won set to: " + p1Won);
             playerTurn = !playerTurn;
         } else {
             b.setText("O");
             b.setTextColor(res.getColor(R.color.colorPlayerTwo));
             p2Btns.add(id);
+            p2Won = hasWon(p2Btns);
+            Log.i(TAG,"p2won set to: " + p2Won);
             playerTurn = !playerTurn;
         }
     }
